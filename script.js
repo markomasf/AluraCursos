6e8dfc705d13fe5daaba258041fc32dbfb9cfb7f
Marco Antonio <markomasf@gmail.com>
2022-10-12 11:54:59 -0300

    Aula 2 - Script

---
+++ script.js
@@ -0,0 +1,20 @@
+//var (uso mais liberado)
+/* var altura = 5;
+var comprimento = 7;
+area = altura * comprimento;
+console.log(area);
+var area; */
+
+// Let
+
+let forma = 'retângulo'
+let altura = 9;
+let comprimento = 7;
+
+let area;
+if (forma === 'retângulo') {
+    area = altura * comprimento;
+} else {
+
+}
+console.log(area);
