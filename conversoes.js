43d4de80dad2bc2b0a289032e3294f49d77c11a5
Marco Antonio <markomasf@gmail.com>
2022-10-12 12:18:24 -0300

    Aula 2 - Conversões

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dados
+// booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero + numeroString)
+
+// conversão explícita
+
+console.log(numero + Number(numeroString))
